33ee696e8f15dca4240b58a0b6ace3cd4351f9fc
Shalini <shalini.m@mitrahsoft.in>
2026-08-11 19:56:45 +0530

    changes made in entity

--- src/main/java/com/example/notificationservice/entity/Notification.java
+++ src/main/java/com/example/notificationservice/entity/Notification.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -22,35 +23,22 @@ public class Notification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "customer_id", nullable = false)
+    @Column(nullable = false)
     private Long customerId;
 
-    @Column(name = "order_id")
     private Long orderId;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false)
     private String type;
 
-    @Column(length = 200)
     private String title;
 
-    @Column(length = 500)
     private String message;
 
-    @Builder.Default
-    @Column( nullable = false)
+    @Column(nullable = false)
     private Boolean isRead = false;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
 
-    @PrePersist
-    void onCreate() {
-        if (createdAt == null) {
-            createdAt = LocalDateTime.now();
-        }
-        if (isRead == null) {
-            isRead = false;
-        }
-    }
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
