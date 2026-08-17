4fc2dfa3d6869ec54a86f20d6004bb66b8233e45
Shalini <shalini.m@mitrahsoft.in>
2026-08-17 16:51:46 +0530

    Implemented a actuator and swagger

--- src/main/java/com/example/notificationservice/NotificationserviceApplication.java
+++ src/main/java/com/example/notificationservice/NotificationserviceApplication.java
@@ -1,9 +1,16 @@
 package com.example.notificationservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Notification Service API",
+		version = "v1",
+		description = "Notification APIs for the Order Management System"
+))
 public class NotificationserviceApplication {
 
 	public static void main(String[] args) {
