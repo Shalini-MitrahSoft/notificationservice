958e4f7669ba211551078ba941da764dba39db4e
Shalini <shalini.m@mitrahsoft.in>
2026-08-17 19:37:59 +0530

    Implemented a swagger

--- src/main/java/com/example/notificationservice/NotificationserviceApplication.java
+++ src/main/java/com/example/notificationservice/NotificationserviceApplication.java
@@ -2,6 +2,7 @@ package com.example.notificationservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		title = "Notification Service API",
 		version = "v1",
 		description = "Notification APIs for the Order Management System"
-))
+), servers = @Server(url = "/", description = "Current host"))
 public class NotificationserviceApplication {
 
 	public static void main(String[] args) {
